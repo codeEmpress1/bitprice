975cd670789cad03be7e65e12b681c8cc4013b07
codeEmpress1 <halof.halima@gmail.com>
2020-06-19 10:21:43 +0100

    updates index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
-import Layout from '../components/layout'
+import Fetch from 'isomorphic-unfetch';
+import Layout from '../components/Layout'
+import Prices from '../components/Prices'
 
-const Index = () => (
+const Index = (props) => (
     <Layout>
         <div>
-            <h1>Index page</h1>
+            <h1>WELCOME!!!</h1>
+            <p>Check current bitcoin rate</p>
+            <Prices bpi = {props.bpi}/>
         </div>
     </Layout>
 )
 
+Index.getInitialProps = async function () {
+    const result = await Fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+    const data = await result.json();
+
+    return {
+        bpi: data.bpi
+    };
+}
 export default Index
